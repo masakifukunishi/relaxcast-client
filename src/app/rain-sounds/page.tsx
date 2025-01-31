c7c5d387f9f8a62826c9fbb013ffeaf06e181ca7
MasakiFukunishi <fmsknn@gmail.com>
2025-01-31 21:37:53 +0900

    add rain sounds

---
+++ src/app/rain-sounds/page.tsx
@@ -0,0 +1,18 @@
+import AboutSection from "@/app/features/player/components/AboutSection";
+import PlayerSection from "@/app/features/player/components/PlayerSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rain Sound | Minimal Tune",
+};
+
+const RainSound = () => {
+  return (
+    <>
+      <PlayerSection channel="rain-sounds" />
+      <AboutSection channelName="Rain Sounds" />
+    </>
+  );
+};
+
+export default RainSound;
