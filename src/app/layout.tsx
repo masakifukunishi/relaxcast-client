34a82b2b23a6a4795b27779b0e285255c71dfb69
MasakiFukunishi <fmsknn@gmail.com>
2025-02-08 23:18:56 +0900

    change app name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Relax Tunes",
+  title: "RelaxCast",
   description: "An online streaming station for relaxing sounds",
 };
 
