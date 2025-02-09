5e61da8cb9c46b7d9d0481975e0de7068041b12a
MasakiFukunishi <fmsknn@gmail.com>
2025-02-10 00:30:07 +0900

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import GoogleAdsense from "@/features/common/GoogleAdsense";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,9 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!;
+  const adsenseId = process.env.NEXT_PUBLIC_ADSENSE_ID!;
+
   return (
     <html lang="en">
       <body className={`antialiased`}>{children}</body>
+      <GoogleAdsense adsenseId={adsenseId} />
       <GoogleAnalytics gaId={gaId} />
     </html>
   );
