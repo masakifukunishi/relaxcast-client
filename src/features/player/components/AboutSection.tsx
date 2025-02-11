e615f25523ab26778c572474dcf2d63222631aab
MasakiFukunishi <fmsknn@gmail.com>
2025-02-11 10:16:41 +0900

    fix description

--- src/features/player/components/AboutSection.tsx
+++ src/features/player/components/AboutSection.tsx
@@ -17,8 +17,7 @@ const AboutSection = ({ channelName }: AboutSectionProps) => {
         <div className="mb-16">
           <h2 className="text-2xl font-bold mb-4">What is RelaxCast?</h2>
           <p className="text-lg leading-relaxed text-stone-300">
-            RelaxCast offers streaming channels with relaxing sounds to help you unwind. All channels are available 24/7, always here to
-            bring you warmth and comfort.
+            RelaxCast offers 24/7 streaming channels featuring relaxing sounds to help you unwind.
           </p>
         </div>
         <div>
